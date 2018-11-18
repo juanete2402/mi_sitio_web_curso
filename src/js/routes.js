cbcda366922df5b8434a4beeb9f4369167d44dbf
Juan Montoya <jcmv2402@gmail.com>
2018-11-18 17:40:55 -0500

    todo hecho hasta logueo y deslogueo

---
+++ src/js/routes.js
@@ -0,0 +1,53 @@
+// dependencias
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
+import './config'
+import {signIn, signOut} from './auth'
+
+// templates
+import tplHome from '../html/home.tpl.html'
+import tplAbout from '../html/about.tpl.html'
+import tplContact from '../html/contact.tpl.html'
+import tplAdmin from '../html/admin.tpl.html'
+import tplAdminAuth from '../html/admin_auth.tpl.html'
+
+const routes = () => {
+
+    const d = document
+    const main = d.querySelector('.Main')
+    // cargar otra pagina html
+    d.addEventListener('DOMContentLoaded', e => {
+        main.innerHTML = tplHome
+    })
+
+    // click
+    d.addEventListener('click', e => {
+        if (e.target.matches('a[href="#"]')) {
+            e.preventDefault()
+        }
+
+        if (e.target.matches('#home')) {
+            main.innerHTML = tplHome
+        } else if (e.target.matches('#about')) {
+            main.innerHTML = tplAbout
+        } else if (e.target.matches('#contact')) {
+            main.innerHTML = tplContact
+        } else if (e.target.matches('#admin')) {
+            firebase.auth().onAuthStateChanged(user => {
+                if (user) {
+                    main.innerHTML = tplAdminAuth
+                } else {
+                    main.innerHTML = tplAdmin
+                }
+            })
+        } else if (e.target.matches('#login')) {
+            signIn()
+        } else if (e.target.matches('#logout')) {
+            signOut()
+        }
+    })
+}
+
+export default routes
